b206199de7620ac85556ca158c9b63f65d717ada
Jeyoung-Park <jason9380@naver.com>
2021-12-30 02:06:47 +0900

    fix: 에러 처리 미들웨어 개선

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ nunjucks.configure("views", {
 });
 
 app.use(morgan("dev"));
-app.use("/", express.static(path.join(__dirname, 'public')));
+app.use("/", express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
@@ -40,7 +40,9 @@ app.use("/", indexRouter);
 app.use("/info", infoRouter);
 
 app.use((req, res, next) => {
-  res.status(404).send("Not Found");
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
 });
 
 app.use((err, req, res, next) => {
