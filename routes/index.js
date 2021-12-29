2822d242520a42b7fb6cdb98de4f07994a4a6a83
Jeyoung-Park <jason9380@naver.com>
2021-12-30 02:02:29 +0900

    feat: view 추가

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 // GET / 라우터
 router.get("/", (req, res) => {
-  res.send("Computer License");
+  res.render("index", { title: "Computer License" });
 });
 
 module.exports = router;
