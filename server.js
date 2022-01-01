20e4eae9f069925d6dcf51f0fdba0e1abf2effc5
Jeyoung-Park <jason9380@naver.com>
2022-01-01 21:39:48 +0900

    fix: server.js 로그 추가

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
-const app = require('./app');
+const app = require("./app");
+const dotenv = require("dotenv");
+dotenv.config();
 
-app.listen(app.get('port'), () => {
-  console.log(app.get('port'), '번 포트에서 대기중');
+app.listen(app.get("port"), () => {
+  console.log(
+    app.get("port"),
+    "번 포트에서 대기중",
+    process.env.USERNAME,
+    process.env.SEQUELIZE_PASSWORD,
+    process.env.HOST,
+    process.env.COOKIE_SECRET
+  );
 });
