d8700d5d6bbf984de6078d82e98ee8532b863fba
Jeyoung-Park <jason9380@naver.com>
2022-01-01 17:35:23 +0900

    fix: env 변수 변경

--- config/config.js
+++ config/config.js
@@ -1,26 +1,31 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
+export const USERNAME = process.env._USERNAME;
+export const SEQUELIZE_PASSWORD = process.env._SEQUELIZE_PASSWORD;
+export const HOST = process.env._HOST;
+export const COOKIE_SECRET = process.env._COOKIE_SECRET;
+
 module.exports = {
   development: {
-    username: process.env.USERNAME,
-    password: process.env.SEQUELIZE_PASSWORD,
+    username: USERNAME,
+    password: SEQUELIZE_PASSWORD,
     database: "computerlicense",
-    host: process.env.HOST,
+    host: HOST,
     dialect: "mysql",
   },
   test: {
-    username: process.env.USERNAME,
-    password: process.env.SEQUELIZE_PASSWORD,
+    username: USERNAME,
+    password: SEQUELIZE_PASSWORD,
     database: "computerlicense",
-    host: process.env.HOST,
+    host: HOST,
     dialect: "mysql",
   },
   production: {
-    username: process.env.USERNAME,
-    password: process.env.SEQUELIZE_PASSWORD,
+    username: USERNAME,
+    password: SEQUELIZE_PASSWORD,
     database: "computerlicense",
-    host: process.env.HOST,
+    host: HOST,
     dialect: "mysql",
     logging: false,
   },
